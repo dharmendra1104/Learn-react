9bb4ad75d1adff5c10d60345db1d60f67b67c96d
dharmendra1104 <dharmendra11042@gmail.com>
2024-05-04 18:13:47 +0300

    Update

---
+++ New folder/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import PasswaordValid from './component/PasswaordValid'
+// import Prctice from './component/Prctice'
+// import Random from './component/Random'
+
+
+function App() {
+
+  return (
+    <>
+       {/* <Prctice/> */}
+       {/* <Random/> */}
+       <PasswaordValid/>
+
+    </>
+  )
+}
+
+export default App
